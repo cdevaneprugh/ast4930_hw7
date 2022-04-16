3219cf6b9361b3af2c577affcf28fbc7951880ce
Cooper DeVane-Prugh <cdevaneprugh@c0709a-s30.ufhpc>
2022-04-16 15:54:35 -0400

    final submission for homework seven

--- devaneprugh_cooper_hw7.py
+++ devaneprugh_cooper_hw7.py
@@ -1,54 +1,41 @@
-# when installing miniconda, I used >sh *miniconda file* instead of >./*miniconda file*
-# installed astropy, matplotlib, and numpy with conda install *package*
-
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy import units as u
 
 # load in sed textfile
 # first column is wavelength (micron)
-# second column is specific luminosity (L Sun/micron)
+# second column is specific luminosity (L Sun/microns)
 data = np.loadtxt('sed.txt', delimiter = ',')
 
-# print all data in left hand column
-#print(data[:,0])
+# sort data by increasing wavelength
+sorted_data = np.sort(data)[::-1]
 
-# graph full spectrum
+# set bounds for integration
+wavelength = sorted_data[:,0]
+data_to_integrate = sorted_data[np.where((wavelength > 10) & (wavelength < 1000))]
 
-x = data[:,0]
-y = data[:,1]
+# add units
+wavelength = data_to_integrate[:,0] * u.micron
+luminosity = data_to_integrate[:,1] * u.Lsun / u.micron
 
+# convert luminosity to erg/s/micron
+luminosity = luminosity.to(u.erg/u.s/u.micron)
+
+# integrate to obtain total luminosity
+total_luminosity = np.trapz(luminosity, wavelength)
+print('Total luminosity = ', '{:.3e}'.format(total_luminosity))
+
+# plot spectrum and save image
+wav = sorted_data[:,0]
+lum = sorted_data[:,1]
+
+plt.style.use('dark_background')
 plt.figure(figsize=(9,6))
-plt.plot(x, y, color = 'k')
-plt.title('Spectral Energy Distribution')
-plt.xlabel('Wavelength')
-plt.ylabel('Luminosity')
+plt.plot(wav, lum, color='r')
+plt.title('Spectral Energy Distribution', fontsize=14)
+plt.xlabel('Wavelength [$\mu m$]', fontsize=12)
+plt.ylabel('$L_{\odot}$ / $\mu m$', fontsize=12)
 plt.xscale('log')
 plt.yscale('log')
 
-#plt.savefig('full spectrum', dpi=300)
-
-# create mask to limit wavelength
-mask = (data[:,0] >= 10) & (data[:,0] <= 10000)
-
-# create new array of masked data
-new_data = data[mask]
-
-# print and look at how much data was reduced
-#print('Number of data points removed =',len(data)-len(new_data))
-#print(new_data)
-
-# plot new data
-x = new_data[:,0]
-y = new_data[:,1]
-
-# graph limited python
-plt.figure(figsize=(9,6))
-plt.plot(x,y, color='b')
-plt.title('Limited Distribution')
-plt.xlabel('Wavelength')
-plt.ylabel('Luminosity')
-plt.xscale('log')
-plt.yscale('log')
-
-#plt.savefig('reduced spectrum', dpi=300)
+plt.savefig('devaneprugh_cooper_hw7.png', dpi=300)
